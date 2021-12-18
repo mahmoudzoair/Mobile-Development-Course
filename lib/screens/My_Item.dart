049efffd3ab6b8c76c65be5d38af4c58b457b390
mahmoud zoair <73954049+mahmoudzoair@users.noreply.github.com>
2021-12-18 18:50:42 +0200

    My Items widgets and screens

---
+++ lib/screens/My_Item.dart
@@ -0,0 +1,24 @@
+import 'package:exchange_app/models/item.dart';
+import 'package:flutter/material.dart';
+import 'package:exchange_app/stateless_widgets/bar.dart';
+import 'package:exchange_app/statefull_widgets/My_Items.dart';
+import 'package:exchange_app/statefull_widgets/Product_Description.dart';
+
+
+class My_Products extends StatelessWidget {
+  const My_Products({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: startDrawer(),
+      appBar: BaseAppBar(
+        backgroundColor: Colors.blue,
+        title: Text('exchange'),
+        appBar: AppBar(),
+
+      ),
+      body: MyHomePage(title: 'kkk',),
+    );
+  }
+}
---
+++ lib/statefull_widgets/My_Items.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:exchange_app/models/item.dart';
+import 'package:exchange_app/statefull_widgets/Product_Description.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: ListView.builder(
+            padding: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 10),
+            itemCount: items[0].id,
+            itemBuilder: (BuildContext ctxt, int index) {
+              return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MyProducts(items[index])),
+                    );
+                  },
+                  child: Card(
+                    child: ListTile(
+                      title: Text(items[index].title),
+                      subtitle: Text(
+                        items[index].description,
+                      ),
+                    ),
+                  ));
+            }));
+  }
+}
